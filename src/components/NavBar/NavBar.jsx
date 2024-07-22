99055af9e5299297e555704adada2a5de13af465
ForgeDeveloper <Techforgedeveloper@gmail.com>
2024-07-21 23:34:35 -0700

    ReadPage Added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -72,10 +72,10 @@ export default function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/read"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-[#76E5BD] dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  About
+                  Read
                 </a>
               </li>
               <li>
